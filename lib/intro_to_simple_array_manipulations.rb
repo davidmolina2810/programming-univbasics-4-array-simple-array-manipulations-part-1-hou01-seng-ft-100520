8656a5b00d46331c178d3caba6d772cec7975029
David Molina <davidmolina2810@gmail.com>
2020-08-08 22:21:26 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,19 @@
+#method that takes a string and an array and .push string into array
+def using_push(array, str)
+  array.push(str)
+end
+
+#method that takes a string and an arrray and .unshift(str) into array
+def using_unshift(array, str)
+  array.unshift(str)
+end
+
+#method that takes array as arg and .pop the last element
+def using_pop(array)
+  array.pop
+end
+
+#method that accepts array as arg and .pop last 2 elements in array
+def pop_with_args(array)
+  array.pop(2)
+end
